452f7d6e32bc3a152df966ff1a1c8547d93cb7e8
Nallamothu-venugopal <nallamothuvenugopal@gmail.com>
2023-08-18 09:23:25 +0530

    email auth admindto

--- ATM-BS.API/DTOS/AdminDTO.cs
+++ ATM-BS.API/DTOS/AdminDTO.cs
@@ -9,10 +9,10 @@ namespace ATM_BS.API.DTOS
         [Required(ErrorMessage = "Admin Name is required")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Password is required")]
-        [RegularExpression("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,16}")]
+        [RegularExpression("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,16}",ErrorMessage ="Password must contain an uppercase alphabet, a lower case alphabet, a number, a special character and the length should be in between 8 and 16(inclusive)")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage ="Invalid EmailId")]
+        [RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$",ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
     }
 }
