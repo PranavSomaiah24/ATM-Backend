706931b45f3d28e94a8ce4f508dd708662dc23e7
Nallamothu-venugopal <nallamothuvenugopal@gmail.com>
2023-08-18 12:49:21 +0530

    Fund Transfer Backend

---
+++ ATM-BS.API/DTOS/FundTransferDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ATM_BS.API.DTOS
+{
+    public class FundTransferDTO
+    {
+
+        [Required(ErrorMessage = "From account Number is required")]
+        [RegularExpression("[0-9]{8}", ErrorMessage = "Account Number should be 8 digits long")]
+        public int FromAccountNumber { get; set; }
+
+        [Required(ErrorMessage = "To account Number is required")]
+        [RegularExpression("[0-9]{8}", ErrorMessage = "Account Number should be 8 digits long")]
+        public int ToAccountNumber { get; set;}
+        [Required(ErrorMessage = "Amount to be transferred is required")]
+        public double Amount { get; set; }
+    }
+}
