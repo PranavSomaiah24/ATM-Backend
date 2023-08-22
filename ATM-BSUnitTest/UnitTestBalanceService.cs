08b12a424f751270c60c417fd2cf204e88e282f5
Nishtha2709 <nish279rs@gmail.com>
2023-08-22 14:53:33 +0530

    Test units in process

---
+++ ATM-BSUnitTest/UnitTestBalanceService.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using ATM_BS.API.Controllers;
+using ATM_BS.API.Entities;
+using ATM_BS.API.Service;
+
+namespace ATM_BSUnitTest
+{
+    public class UnitTestBalanceService
+    {
+        private readonly Mock<IBalanceService> balanceService;
+        public UnitTestBalanceService()
+        {
+            balanceService = new Mock<IBalanceService>();
+
+        }
+
+        [Fact]
+        public void Test_AddBalance()
+        {
+
+        }
+
+        [Fact]
+        public void Test_EditBalance()
+        {
+
+        }
+
+        [Fact]
+        public void Test_GetBalance()
+        {
+            var balanceList = GetBalanceData();
+            balanceService.Setup(x => x.GetBalance(12345678)).Returns(balanceList[1]);
+            var result = balanceService.Object;
+            var balanceResult = result.GetBalance(12345678);
+            Assert.NotNull(balanceResult);
+            Assert.Equal(balanceList[0].AccountNumber, balanceResult.AccountNumber);
+            Assert.True(balanceList[1].AccountNumber == balanceResult.AccountNumber);
+        }
+
+        private List<Balance> GetBalanceData()
+        {
+            List<Balance> balanceData = new List<Balance>
+            {
+                new Balance
+                {
+                    AccountNumber = 12345678,
+                    AccountBalance = 3000
+                },
+                new Balance
+                {
+                    AccountNumber = 12345679,
+                    AccountBalance = 4000
+                },
+                new Balance
+                {
+                    AccountNumber = 65473621,
+                    AccountBalance = 0
+                }
+                
+            };
+            return balanceData;
+        }
+    }
+}
---
+++ ATM-BSUnitTest/UnitTestTransactionService.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using ATM_BS.API.Controllers;
+using ATM_BS.API.Entities;
+using ATM_BS.API.Service;
+
+
+namespace ATM_BSUnitTest
+{
+    public class UnitTestTransactionService
+    {
+        private readonly Mock<ITransactionService> transactionService;
+        public UnitTestTransactionService()
+        {
+            transactionService = new Mock<ITransactionService>();
+
+        }
+    }
+}
