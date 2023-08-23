23422278101eb5b8eb85efc675f38bcaf31e8e03
Nallamothu-venugopal <nallamothuvenugopal@gmail.com>
2023-08-23 11:11:25 +0530

    entity change and testing update

---
+++ ATM-BS.API/Migrations/20230823052205_Migration55.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATM_BS.API.Migrations
+{
+    public partial class Migration55 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
